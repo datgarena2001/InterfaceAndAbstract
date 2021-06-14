bbcc2364e953fb407b0edd0430d3070adb882e9f
datgarena2001 <81565543+datgarena2001@users.noreply.github.com>
2021-06-14 15:36:21 +0700

    Add files via upload

---
+++ AirPlane.java
@@ -0,0 +1,17 @@
+package advance.dev;
+
+public class AirPlane extends Machine implements Flyable{
+
+	@Override
+	void fuel() {
+		// TODO Auto-generated method stub
+		System.out.println("May bay do nguyen lieu");
+	}
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		System.out.println("May bay bay tren troi");
+	}	
+
+}
